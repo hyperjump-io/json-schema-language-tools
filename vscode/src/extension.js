1ab3dab982e02d079e6f621d8929d7568d11fe1a
D V <divyadityasnaruka@gmail.com>
2024-03-11 09:45:10 +0530

    update extension.js

--- vscode/src/extension.js
+++ vscode/src/extension.js
@@ -1,31 +1,31 @@
 "use strict";
 const path = require("node:path");
-const { LanguageClient } = require("vscode-languageclient/node.js");
+const { LanguageClient, TransportKind } = require("vscode-languageclient/node.js");
 
 
 let client;
 
 const activate = (context) => {
-  // const serverModule = context.asAbsolutePath(path.join("out", "server.js"));
-  // const serverOptions = {
-  //   run: {
-  //     module: serverModule,
-  //     transport: TransportKind.ipc
-  //   },
-  //   debug: {
-  //     module: serverModule,
-  //     transport: TransportKind.ipc,
-  //     options: {
-  //       execArgc: ["--nolazy", "--inspect=6009"]
-  //     }
-  //   }
-  // };
+  const serverModule = context.asAbsolutePath(path.join("out", "server.js"));
+  const serverOptions = {
+    run: {
+      module: serverModule,
+      transport: TransportKind.ipc
+    },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: {
+        execArgc: ["--nolazy", "--inspect=6009"]
+      }
+    }
+  };
 
   // Temporary Hack. Get the above code working.
-  const serverOptions = {
-    command: "node",
-    args: [context.asAbsolutePath(path.join("..", "language-server", "src", "server.js")), "--stdio"]
-  };
+  // const serverOptions = {
+  //   command: "node",
+  //   args: [context.asAbsolutePath(path.join("..", "language-server", "src", "server.js")), "--stdio"]
+  // };
 
   const clientOptions = {
     documentSelector: [
