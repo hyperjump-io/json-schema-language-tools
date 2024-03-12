20ee596b688061ae46f4e8a44d92913e765a2942
Jason Desrosiers <jdesrosi@gmail.com>
2024-03-12 13:04:52 -0700

    Fix vscode build

--- vscode/src/extension.js
+++ vscode/src/extension.js
@@ -1,30 +1,24 @@
 "use strict";
 const path = require("node:path");
-const { LanguageClient } = require("vscode-languageclient/node.js");
+const { LanguageClient, TransportKind } = require("vscode-languageclient/node.js");
 
 
 let client;
 
 const activate = (context) => {
-  // const serverModule = context.asAbsolutePath(path.join("out", "server.js"));
-  // const serverOptions = {
-  //   run: {
-  //     module: serverModule,
-  //     transport: TransportKind.ipc
-  //   },
-  //   debug: {
-  //     module: serverModule,
-  //     transport: TransportKind.ipc,
-  //     options: {
-  //       execArgc: ["--nolazy", "--inspect=6009"]
-  //     }
-  //   }
-  // };
-
-  // Temporary Hack. Get the above code working.
+  const serverModule = context.asAbsolutePath(path.join("out", "server.js"));
   const serverOptions = {
-    command: "node",
-    args: [context.asAbsolutePath(path.join("..", "language-server", "src", "server.js")), "--stdio"]
+    run: {
+      module: serverModule,
+      transport: TransportKind.ipc
+    },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: {
+        execArgc: ["--nolazy", "--inspect=6009"]
+      }
+    }
   };
 
   const clientOptions = {
