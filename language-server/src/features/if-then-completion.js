52d5ec04313e50a675abffdb504d763502a99268
Diya <diya.solanki.31@gmail.com>
2024-06-05 22:44:44 +0530

    Refactor for if/then completion

--- language-server/src/features/if-then-completion.js
+++ language-server/src/features/if-then-completion.js
@@ -1,4 +1,5 @@
 import { CompletionItemKind } from "vscode-languageserver";
+import * as SchemaDocument from "../schema-document.js";
 import { subscribe } from "../pubsub.js";
 
 
@@ -8,61 +9,63 @@ export default {
   },
 
   onInitialized() {
-    subscribe("completions", async (_message, { document, offset, completions }) => {
-      const text = document.getText();
-      const startOfLine = text.lastIndexOf("\n", offset - 1) + 1;
-      const textBeforePosition = text.slice(startOfLine, offset);
-      if (textBeforePosition.trim().endsWith("\"if\":")) {
-        completions.push(...ifThenPatternCompletion());
+    subscribe("completions", async (_message, { schemaDocument, offset, completions }) => {
+      const currentProperty = SchemaDocument.findNodeAtOffset(schemaDocument, offset);
+      if (currentProperty && currentProperty.pointer.endsWith("/if")) {
+        completions.push(...ifThenPatternCompletion);
       }
+
+      // const text = document.getText();
+      // const startOfLine = text.lastIndexOf("\n", offset - 1) + 1;
+      // const textBeforePosition = text.slice(startOfLine, offset);
+      // if (textBeforePosition.trim().endsWith("\"if\":")) {
+      //   completions.push(...ifThenPatternCompletion());
+      // }
     });
   }
 };
 
-function ifThenPatternCompletion() {
-  return [
-    {
-      label: "if/then",
-      kind: CompletionItemKind.Snippet,
-      insertText: `{
-          "type": "object",
-          "properties": {
-            "{varName}": { "const": "{value}" }
-          },
-          "required": ["{varName}"]
-        },
-        
-        "then": {
-        }`,
-      documentation: "Basic if/then pattern with a single condition and corresponding schema."
+const ifThenPatternCompletion = [
+  {
+    label: "if/then",
+    kind: CompletionItemKind.Snippet,
+    insertText: `{
+      "type": "object",
+      "properties": {
+        "{varName}": { "const": "{value}" }
+      },
+      "required": ["{varName}"]
     },
-    {
-      label: "If/then/else",
-      kind: CompletionItemKind.Snippet,
-      insertText: `{
-           "type": "object",
-           "properties": {
-            "{varName}": { "const": "{value}" }
-          },
-          "required": ["{varName}"]
-         },
-         "then": {
-         },
-         "else": {
-         }`,
-      documentation: "Conditional object structure with if/then/else logic"
+    "then": {
+    }`,
+    documentation: "Basic if/then pattern with a single condition and corresponding schema."
+  },
+  {
+    label: "If/then/else",
+    kind: CompletionItemKind.Snippet,
+    insertText: `{
+      "type": "object",
+      "properties": {
+        "{varName}": { "const": "{value}" }
+      },
+      "required": ["{varName}"]
     },
-    {
-      label: "true",
-      kind: CompletionItemKind.Snippet,
-      insertText: `true`,
-      documentation: "if true"
+    "then": {
     },
-    {
-      label: "false",
-      kind: CompletionItemKind.Snippet,
-      insertText: `false`,
-      documentation: "if false"
-    }
-  ];
-}
+    "else": {
+    }`,
+    documentation: "Conditional object structure with if/then/else logic"
+  },
+  {
+    label: "true",
+    kind: CompletionItemKind.Snippet,
+    insertText: `true`,
+    documentation: "if true"
+  },
+  {
+    label: "false",
+    kind: CompletionItemKind.Snippet,
+    insertText: `false`,
+    documentation: "if false"
+  }
+];
