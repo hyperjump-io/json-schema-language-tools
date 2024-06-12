823d799c11d25d389400f1dc792facf6f315e1ac
Jason Desrosiers <jdesrosi@gmail.com>
2024-06-11 20:32:24 -0700

    Get schema completion test working

--- language-server/src/features/schema-completion.test.js
+++ language-server/src/features/schema-completion.test.js
@@ -1,8 +1,8 @@
 import { beforeAll, describe, expect, test } from "vitest";
-import { clientCapabilities, getTestClient } from "../test-utils.js";
-import { CompletionRequest, InitializeRequest, InitializedNotification } from "vscode-languageserver";
+import { CompletionItemKind, CompletionRequest, InitializeRequest, InitializedNotification } from "vscode-languageserver/node.js";
 import completion from "./completion.js";
 import schemaCompletion from "./schema-completion.js";
+import { clientCapabilities, getTestClient, openDocument } from "../test-utils.js";
 
 
 describe("Feature - $schema completion", () => {
@@ -12,18 +12,23 @@ describe("Feature - $schema completion", () => {
     client = getTestClient([completion, schemaCompletion]);
     const init = {
       capabilities: clientCapabilities,
-      workspaceFolders: ["./src/test-workspace"]
+      workspaceFolders: []
     };
     await client.sendRequest(InitializeRequest.type, init);
     await client.sendNotification(InitializedNotification);
   });
 
   test("$schema completion", async () => {
+    const documentUri = "file://path/to/workspace/subject.schema.json";
+    await openDocument(client, documentUri, `{
+  "$schema": ""
+}`);
+
     /**
      * @type {import("vscode-languageserver/node.js").CompletionParams}
      */
     const params = {
-      textDocument: { uri: "./src/test-workspace/test.schema.json" },
+      textDocument: { uri: documentUri },
       position: {
         line: 1,
         character: 14
@@ -31,6 +36,27 @@ describe("Feature - $schema completion", () => {
     };
 
     const response = await client.sendRequest(CompletionRequest.type, params);
-    expect(response).to.equal(null);
+    expect(response).to.eql([
+      {
+        kind: CompletionItemKind.Value,
+        label: "https://json-schema.org/draft/2020-12/schema"
+      },
+      {
+        kind: CompletionItemKind.Value,
+        label: "https://json-schema.org/draft/2019-09/schema"
+      },
+      {
+        kind: CompletionItemKind.Value,
+        label: "http://json-schema.org/draft-07/schema#"
+      },
+      {
+        kind: CompletionItemKind.Value,
+        label: "http://json-schema.org/draft-06/schema#"
+      },
+      {
+        kind: CompletionItemKind.Value,
+        label: "http://json-schema.org/draft-04/schema#"
+      }
+    ]);
   });
 });
