d1c2297b72bbaea8892290d1dd7a0324c7e402b5
Jason Desrosiers <jdesrosi@gmail.com>
2024-06-19 17:31:22 -0700

    Add neovim specific workspace tests

---
+++ language-server/src/features/workspace-neovim.test.js
@@ -0,0 +1,93 @@
+import { afterAll, beforeAll, describe, expect, test } from "vitest";
+import { getTestClient, initializeServer, setupWorkspace, tearDownWorkspace } from "../test-utils.js";
+import {
+  PublishDiagnosticsNotification,
+  WorkDoneProgress,
+  WorkDoneProgressCreateRequest
+} from "vscode-languageserver/node.js";
+import { utimes } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import { resolveIri } from "@hyperjump/uri";
+import workspace from "./workspace.js";
+import documentSettings from "./document-settings.js";
+import schemaRegistry from "./schema-registry.js";
+
+
+describe("Feature - workspace (neovim)", () => {
+  let client;
+  let capabilities;
+  let workspaceFolder;
+
+  beforeAll(async () => {
+    client = getTestClient([workspace, documentSettings, schemaRegistry]);
+
+    workspaceFolder = await setupWorkspace({
+      "subject.schema.json": `{ "$schema": "https://json-schema.org/draft/2020-12/cshema" }`
+    });
+
+    /**
+     * @type {import("vscode-languageserver/node.js").InitializeParams}
+     */
+    const init = {
+      capabilities: {
+        workspace: {
+          didChangeWatchedFiles: {
+            dynamicRegistration: false
+          }
+        }
+      },
+      workspaceFolders: [
+        { uri: workspaceFolder }
+      ]
+    };
+    capabilities = await initializeServer(client, init);
+  });
+
+  afterAll(async () => {
+    await client.dispose();
+    await tearDownWorkspace(workspaceFolder);
+  });
+
+  test("capabilities", async () => {
+    expect(capabilities.workspace).to.eql({
+      workspaceFolders: {
+        changeNotifications: true,
+        supported: true
+      }
+    });
+  });
+
+  test("a change to a watched file should validate the workspace", async () => {
+    const validatedSchemas = new Promise((resolve) => {
+      let schemaUris;
+
+      client.onRequest(WorkDoneProgressCreateRequest, ({ token }) => {
+        client.onProgress(WorkDoneProgress, token, ({ kind }) => {
+          if (kind === "begin") {
+            schemaUris = [];
+          } else if (kind === "end") {
+            resolve(schemaUris);
+          }
+        });
+      });
+
+      client.onNotification(PublishDiagnosticsNotification, (params) => {
+        schemaUris.push(params.uri);
+      });
+    });
+
+    const subjectSchemaUri = resolveIri("./subject.schema.json", `${workspaceFolder}/`);
+    await touch(fileURLToPath(subjectSchemaUri));
+
+    expect(await validatedSchemas).to.eql([subjectSchemaUri]);
+  });
+
+  test.todo("changing the workspace folders should validate the workspace", () => {
+    // DidChangeWorkspaceFoldersNotification
+  });
+});
+
+const touch = (path) => {
+  const time = new Date();
+  return utimes(path, time, time);
+};
