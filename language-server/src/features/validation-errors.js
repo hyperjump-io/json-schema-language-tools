3955025f616212c2b11efb8b2470d78e157a5ee5
Diya <diya.solanki.31@gmail.com>
2024-07-17 23:00:21 +0530

    Tests for Validation errors

--- language-server/src/features/validation-errors.js
+++ language-server/src/features/validation-errors.js
@@ -77,7 +77,7 @@ const toErrorMessage = async function* (error) {
   } else if (error.keyword === "https://json-schema.org/keyword/enum") {
     /** @type unknown[] */
     const enumValue = Browser.value(error.keywordNode);
-    yield `Expected one of: ${enumValue.map((value) => JSON.stringify(value, null, "  ")).join(", ")}`;
+    yield `Expected one of: ${toListMessage(enumValue.map((value) => JSON.stringify(value)))}`;
   } else if (error.keyword === "https://json-schema.org/keyword/exclusiveMaximum") {
     const exclusiveMaximum = Browser.value(error.keywordNode);
     yield `Must be less than ${exclusiveMaximum}`;
@@ -88,7 +88,7 @@ const toErrorMessage = async function* (error) {
     // Skip
   } else if (error.keyword === "https://json-schema.org/keyword/maxItems") {
     const maxItems = Browser.value(error.keywordNode);
-    yield `Too many items. A maximum of ${maxItems} are allowed.`;
+    yield `A maximum of ${maxItems} items are allowed`;
   } else if (error.keyword === "https://json-schema.org/keyword/maxLength") {
     const maxLength = Browser.value(error.keywordNode);
     yield `A maximum of ${maxLength} characters are allowed.`;
@@ -100,13 +100,13 @@ const toErrorMessage = async function* (error) {
     yield `Must be less than or equal to ${maximum}`;
   } else if (error.keyword === "https://json-schema.org/keyword/minItems") {
     const minItems = Browser.value(error.keywordNode);
-    yield `A minimum of ${minItems} are required.`;
+    yield `A minimum of ${minItems} items are required`;
   } else if (error.keyword === "https://json-schema.org/keyword/minLength") {
     const minLength = Browser.value(error.keywordNode);
-    yield `A minimum of ${minLength} characters are required.`;
+    yield `A minimum of ${minLength} characters are required`;
   } else if (error.keyword === "https://json-schema.org/keyword/minProperties") {
     const minProperties = Browser.value(error.keywordNode);
-    yield `A minimum of ${minProperties} properties are required.`;
+    yield `A minimum of ${minProperties} properties are required`;
   } else if (error.keyword === "https://json-schema.org/keyword/minimum") {
     const minimum = Browser.value(error.keywordNode);
     yield `Must be greater than or equal to ${minimum}`;
@@ -135,13 +135,32 @@ const toErrorMessage = async function* (error) {
     }
   } else if (error.keyword === "https://json-schema.org/keyword/type") {
     const type = Browser.value(error.keywordNode);
-    yield `Expected a(n) ${type}`;
-  } else if (error.keyword === "https://json-schema.org/evaluation/unevaluatedItems") {
+    if (Array.isArray(type)) {
+      yield `Expected ${["a", "o"].includes(type[0][0]) ? "an" : "a"} ${toListMessage(type)}`;
+    } else {
+      yield `Expected ${["a", "o"].includes(type[0]) ? "an" : "a"} ${type}`;
+    }
+  } else if (error.keyword === "https://json-schema.org/keyword/unevaluatedItems") {
     // Skip
-  } else if (error.keyword === "https://json-schema.org/evaluation/unevaluatedProperties") {
+  } else if (error.keyword === "https://json-schema.org/keyword/unevaluatedProperties") {
     // Skip
-  } else if (error.keyword === "https://json-schema.org/evaluation/uniqueItems") {
-    yield `All items must be unique`;
+  } else if (error.keyword === "https://json-schema.org/keyword/uniqueItems") {
+    // const items = SchemaNode.value(error.instanceNode);
+    yield `Expected all items to be unique.`;
+  } else if (error.keyword === "https://json-schema.org/keyword/draft-04/dependencies") {
+    const dependencies = Browser.value(error.keywordNode);
+    for (const property in dependencies) {
+      if (Array.isArray(dependencies[property])) {
+        const required = dependencies[property];
+
+        const object = SchemaNode.value(error.instanceNode);
+        for (const propertyName of required) {
+          if (!(propertyName in object)) {
+            yield `Property "${propertyName}" is required`;
+          }
+        }
+      }
+    }
   } else if (error.keyword === "https://json-schema.org/evaluation/validate") {
     if (Browser.value(error.keywordNode) === false) {
       yield `No value allowed`;
@@ -149,5 +168,18 @@ const toErrorMessage = async function* (error) {
   } else {
     const keyword = error.keywordNode.cursor.split("/").pop();
     yield `Fails JSON Schema constraint '${keyword}'`;
+  }
+};
+
+/** @type (list: string[]) => string */
+const toListMessage = (list) => {
+  if (list.length === 1) {
+    return list[0];
+  } else if (list.length === 2) {
+    return `${list[0]} or ${list[1]}`;
+  } else {
+    const lastItem = list.pop();
+    const result = list.join(", ");
+    return `${result}, or ${lastItem}`;
   }
 };
